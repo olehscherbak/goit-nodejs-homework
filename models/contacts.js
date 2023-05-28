377970e2a9b6ce37dfaaca8bb3e2feb2135e1888
Oleh.Scherbak <oleh.scherbak@ukr.net>
2023-05-28 23:05:22 +0300

    add hw02-express branch

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,55 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const { nanoid } = require("nanoid");
+const path = require("path");
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, "contacts.json");
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+};
 
-const removeContact = async (contactId) => {}
+const getContactById = async (contactId) => {
+  const list = await listContacts();
+  const result = list.find((item) => contactId === item.id);
+  return result || null;
+};
 
-const addContact = async (body) => {}
+const removeContact = async (contactId) => {
+  const list = await listContacts();
+  const index = list.findIndex((item) => contactId === item.id);
+  if (index !== -1) {
+    const [result] = list.splice(index, 1);
+    fs.writeFile(contactsPath, JSON.stringify(list));
+    return result;
+  } else {
+    console.log(`\x1B[31m no contact with id ${contactId}`);
+    return {};
+  }
+};
 
-const updateContact = async (contactId, body) => {}
+const addContact = async (name, email, phone) => {
+  const argList = [...arguments];
+  const argsValid = argList.every((argument) => Boolean(argument));
+
+  if (!argsValid) {
+    console.log("\x1B[31m required arguments are missed");
+    return {};
+  }
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+
+  const list = await listContacts();
+  list.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(list));
+  return newContact;
+};
+
+const updateContact = async (contactId, body) => {};
 
 module.exports = {
   listContacts,
@@ -16,4 +57,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,40 @@
-const express = require('express')
+const express = require("express");
+const router = express.Router();
+const contactsServices = require("../../models/contacts");
 
-const router = express.Router()
+router.get("/", async (req, res, next) => {
+  const result = await contactsServices.listContacts();
+  res.json(result);
+});
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const result = await contactsServices.getContactById(contactId);
+    if (!result) {
+      const error = new Error(`Contact with id: ${contactId} is not found`);
+      error.status = 404;
+      throw error;
+    }
+    res.json(result);
+  } catch (error) {
+    const { status = 500, message = "Server error" } = error;
+    res.status(status).json({
+      message,
+    });
+  }
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.put("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-module.exports = router
+module.exports = router;
