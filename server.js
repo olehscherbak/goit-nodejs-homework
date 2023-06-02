4eeccf0bbcf9f8f866cda6ecb6b1fe337cae4c24
Oleh.Scherbak <oleh.scherbak@ukr.net>
2023-06-03 00:11:29 +0300

    add DB on render.com

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
+const mongoose = require("mongoose");
 const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+mongoose.set("strictQuery", true);
+const { DB_HOST } = process.env;
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+    console.log("Server running. Use our API on port: 3000");
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
 
 // q7urveLP8phR
