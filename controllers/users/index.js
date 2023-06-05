afcf6f04346db7cf9af58ccefce315d94231a6fd
OlehScherbak <103534824+OlehScherbak@users.noreply.github.com>
2023-06-05 08:17:46 +0300

    added user's logout template

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -2,8 +2,10 @@ const { ctrlWrapper } = require("../../decorators");
 
 const register = require("./register");
 const login = require("./login");
+const logout = require("./logout");
 
 module.exports = {
   register: ctrlWrapper(register),
   login: ctrlWrapper(login),
+  logout: ctrlWrapper(logout),
 };
---
+++ controllers/users/logout.js
@@ -0,0 +1,6 @@
+const { User } = require("../../models");
+const { HttpError } = require("../../helpers");
+
+const logout = async (req, res) => {};
+
+module.exports = logout;
