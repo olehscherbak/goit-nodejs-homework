c7d7fc39fd787ad5d881f240c91e107bcaec2643
Oleh.Scherbak <oleh.scherbak@ukr.net>
2023-06-10 18:28:23 +0300

    final refactor

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -2,34 +2,24 @@ const fs = require("fs/promises");
 const path = require("path");
 const Jimp = require("jimp");
 const { User } = require("../../models");
-// const { HttpError } = require("../../helpers");
 
 const updateAvatar = async (req, res) => {
   const { _id } = req.user;
   const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
   const { path: tempUpload, originalname } = req.file;
 
-  await Jimp.read(tempUpload)
+  const filename = `${_id}_${originalname}`;
+  const resultUpload = path.join(avatarsDir, filename);
+
+  Jimp.read(tempUpload)
     .then((file) => {
       return file.resize(255, 255).quality(60).write(tempUpload);
     })
+    .then(() => fs.rename(tempUpload, resultUpload))
     .catch((err) =>
       console.log(`jimp pakage error, can't resize avatar, ${err.message}`)
     );
 
-  // await Jimp.read(tempUpload, (err, file) => {
-  //   if (err) {
-  //     console.log("jimp pakage error, can't resize avatar");
-  //     HttpError(500, "jimp pakage error, can't resize avatar");
-  //   }
-  //   console.log("resizing...");
-  //   file.resize(255, 255).quality(60).write(tempUpload);
-  //   console.log("resized");
-  // });
-
-  const filename = `${_id}_${originalname}`;
-  const resultUpload = path.join(avatarsDir, filename);
-  await fs.rename(tempUpload, resultUpload);
   const avatarURL = path.join("avatars", filename);
   await User.findByIdAndUpdate(_id, { avatarURL });
   res.json({
